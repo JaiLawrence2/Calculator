078e1a8efe55eb6e22d19d081f4306f3e82c0ba4
JaiLawrence <jailon706@icloud.com>
2024-02-13 15:56:08 -0600

    More work on Dynamic Layout - trying to display buttons

--- app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
+++ app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
@@ -3,6 +3,8 @@ package edu.jsu.mcis.cs408.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
@@ -37,7 +39,20 @@ public class MainActivity extends AppCompatActivity {
         int[][] verticals = new int[KEYS_WIDTH][KEYS_HEIGHT];
         int[] buttons = new int[BUTTON_GRID];
 
-        for (int i = 0; i < BUTTON_GRID; i++) {
+
+        ConstraintSet set = new ConstraintSet();
+        TextView tv = new TextView(this);
+        tv.setId(View.generateViewId());
+        tv.setText(R.string.textplaceholder);
+        tv.setTextSize(48);
+
+        set.connect(tv.getId(),ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,0);
+        set.connect(tv.getId(), ConstraintSet.LEFT,ConstraintSet.PARENT_ID, ConstraintSet.LEFT,0);
+        set.connect(tv.getId(),ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP,0);
+        tv.setGravity(Gravity.END);
+        layout.addView(tv);
+
+        for (int i = 0; i < buttons.length; i++) {
             int id = View.generateViewId(); // generate new ID
             Button button = new Button(this);// create new TextView
             button.setId(id);  // assign ID
@@ -47,40 +62,23 @@ public class MainActivity extends AppCompatActivity {
             layout.addView(button); // add to layout
             buttons[i] = id; // store ID to collection
         }
-        ConstraintSet set = new ConstraintSet();
-        TextView tv = new TextView(this);
-        tv.setId(View.generateViewId());
-        tv.setText(R.string.textplaceholder);
-        tv.setTextSize(50);
-
-        set.connect(tv.getId(),ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,0);
-        //set.connect(tv.getId(), ConstraintSet.LEFT,ConstraintSet.PARENT_ID, ConstraintSet.LEFT,0);
-        set.connect(tv.getId(),ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP,0);
-        layout.addView(tv);
-
-        set.clone(layout);
-        for (int id : buttons) {
-            set.connect(id, ConstraintSet.TOP, binding.guideNorth.getId(), ConstraintSet.TOP);
-            set.connect(id, ConstraintSet.BOTTOM, binding.guideSouth.getId(), ConstraintSet.BOTTOM, 8);
-            set.connect(id, ConstraintSet.LEFT, binding.guideWest.getId(), ConstraintSet.LEFT);
-            set.connect(id, ConstraintSet.RIGHT, binding.guideEast.getId(), ConstraintSet.RIGHT);
+        for (int id: buttons){
+            set.connect(id, ConstraintSet.LEFT, binding.guideWest.getId(), ConstraintSet.LEFT,0);
+            set.connect(id, ConstraintSet.RIGHT, binding.guideEast.getId(), ConstraintSet.RIGHT,0);
+            set.connect(id, ConstraintSet.BOTTOM, binding.guideSouth.getId(), ConstraintSet.BOTTOM);
+            set.connect(id, ConstraintSet.TOP, tv.getId(), ConstraintSet.BOTTOM);
 
         }
         for (int row = 0; row < KEYS_HEIGHT; ++row) {
             set.createHorizontalChain(binding.guideWest.getId(), ConstraintSet.LEFT, binding.guideEast.getId(), ConstraintSet.RIGHT, horizontals[row], null, ConstraintSet.CHAIN_PACKED);
-            for (int col = 0; col < KEYS_WIDTH; ++col) {
-                set.createVerticalChain(tv.getId(), ConstraintSet.BOTTOM, binding.guideSouth.getId(), ConstraintSet.BOTTOM, verticals[col], null, ConstraintSet.CHAIN_PACKED);
-            }
         }
 
-        for (int j = 0; j < BUTTON_GRID; ++j) {
-            set.createVerticalChain(ConstraintSet.PARENT_ID, ConstraintSet.TOP,
-                    ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM,
-                    buttons, null, ConstraintSet.CHAIN_PACKED);
-
-
+        for (int col = 0; col < KEYS_WIDTH; ++col) {
+            set.createVerticalChain(tv.getId(), ConstraintSet.BOTTOM, binding.guideSouth.getId(), ConstraintSet.BOTTOM, verticals[col], null, ConstraintSet.CHAIN_PACKED);
         }
+
         set.applyTo(layout);
+        set.clone(layout);
         LayoutParams params = tv.getLayoutParams();
         params.width = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT;
         params.height = LayoutParams.WRAP_CONTENT;
